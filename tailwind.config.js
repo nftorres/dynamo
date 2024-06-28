60b35de258ac64962153f61d62e066517ac4077e
Fabián Torres <nftorresc@gmail.com>
2024-06-28 12:16:45 -0500

    Añadí nuevas themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            keyframes: {
+                "up-down": {
+                    "0%, 100%": { transform: "translateY(-10px)" },
+                    "50%": { transform: "translateY(-70px)" },
+                },
+            },
+            animation: {
+                "up-down": "up-down 12s infinite",
+            },
+        },
+    },
+    plugins: [],
+};
