aaedb6f6970063979aef1659d4b6bf553777ce9f
Fabián Torres <nftorresc@gmail.com>
2024-07-06 15:18:51 -0500

    Eliminé los console.log

--- src/pages/game/providers/ChatProvider.jsx
+++ src/pages/game/providers/ChatProvider.jsx
@@ -1,84 +1,81 @@
-import React, { useState, useEffect, createContext, useContext } from "react";
-import CHAT_CONFIG from "../views/chat/chat.config.js";
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import CHAT_CONFIG from '../views/chat/chat.config.js';
 
 const chatContext = createContext({});
 const chatToggleContext = createContext({});
 
 export function ChatProvider({ children }) {
-    const [messages, setMessages] = useState([
-        CHAT_CONFIG.flow.welcome_message,
-    ]);
-    const [chat, setChat] = useState({
-        config: { ...CHAT_CONFIG },
-        messages: messages,
-    });
+	const [messages, setMessages] = useState([
+		CHAT_CONFIG.flow.welcome_message,
+	]);
+	const [chat, setChat] = useState({
+		config: { ...CHAT_CONFIG },
+		messages: messages,
+	});
 
-    const addMessage = (message = {}) => {
-        setMessages((prevMessages) => [...prevMessages, message]);
-    };
+	const addMessage = (message = {}) => {
+		setMessages((prevMessages) => [...prevMessages, message]);
+	};
 
-    const addUserMessage = (message = {}) => {
-        addMessage(message);
-    };
+	const addUserMessage = (message = {}) => {
+		addMessage(message);
+	};
 
-    const getLastDynamoMessage = () => {
-        const dynamoMessages = messages.filter(
-            (message) => message.meta.sender === "dynamo"
-        );
-        const lastMessage = dynamoMessages[dynamoMessages.length - 1];
-        return lastMessage;
-    };
+	const getLastDynamoMessage = () => {
+		const dynamoMessages = messages.filter(
+			(message) => message.meta.sender === 'dynamo'
+		);
+		const lastMessage = dynamoMessages[dynamoMessages.length - 1];
+		return lastMessage;
+	};
 
-    const deactivateLastDynamoMessage = () => {
-        try {
-            const lastDynamoMessage = getLastDynamoMessage();
+	const deactivateLastDynamoMessage = () => {
+		try {
+			const lastDynamoMessage = getLastDynamoMessage();
 
-            if (lastDynamoMessage && lastDynamoMessage.meta.active) {
-                const updatedMessage = {
-                    ...lastDynamoMessage,
-                    meta: { ...lastDynamoMessage.meta, active: false },
-                };
+			if (lastDynamoMessage && lastDynamoMessage.meta.active) {
+				const updatedMessage = {
+					...lastDynamoMessage,
+					meta: { ...lastDynamoMessage.meta, active: false },
+				};
 
-                setMessages((prevMessages) =>
-                    prevMessages.map((msg) =>
-                        msg === lastDynamoMessage ? updatedMessage : msg
-                    )
-                );
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+				setMessages((prevMessages) =>
+					prevMessages.map((msg) =>
+						msg === lastDynamoMessage ? updatedMessage : msg
+					)
+				);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    const addFollowingDynamoMessage = () => {
-        const lastDynamoMessage = getLastDynamoMessage();
-        const newMessage = CHAT_CONFIG.flow[lastDynamoMessage.meta.next];
-        addMessage(newMessage);
-    };
+	const addFollowingDynamoMessage = () => {
+		const lastDynamoMessage = getLastDynamoMessage();
+		const newMessage = CHAT_CONFIG.flow[lastDynamoMessage.meta.next];
+		addMessage(newMessage);
+	};
 
-    // Temporal -> Muestra la lista de mensajes del chat
-    useEffect(() => {
-        setChat((prevChat) => ({
-            ...prevChat,
-            messages: messages,
-        }));
-        console.log("Se añadió un nuevo mensaje: ", messages);
-    }, [messages]);
+	useEffect(() => {
+		setChat((prevChat) => ({
+			...prevChat,
+			messages: messages,
+		}));
+	}, [messages]);
 
-    return (
-        <chatContext.Provider value={chat}>
-            <chatToggleContext.Provider
-                value={{
-                    addUserMessage: addUserMessage,
-                    addFollowingDynamoMessage: addFollowingDynamoMessage,
-                    deactivateLastDynamoMessage: deactivateLastDynamoMessage,
-                    getLastDynamoMessage: getLastDynamoMessage,
-                }}
-            >
-                {children}
-            </chatToggleContext.Provider>
-        </chatContext.Provider>
-    );
+	return (
+		<chatContext.Provider value={chat}>
+			<chatToggleContext.Provider
+				value={{
+					addUserMessage: addUserMessage,
+					addFollowingDynamoMessage: addFollowingDynamoMessage,
+					deactivateLastDynamoMessage: deactivateLastDynamoMessage,
+					getLastDynamoMessage: getLastDynamoMessage,
+				}}>
+				{children}
+			</chatToggleContext.Provider>
+		</chatContext.Provider>
+	);
 }
 
 export const useChatContext = () => useContext(chatContext);
--- src/pages/game/providers/GameProvider.jsx
+++ src/pages/game/providers/GameProvider.jsx
@@ -1,53 +1,48 @@
-import React, { useState, useEffect, createContext, useContext } from "react";
+import React, { useState, useEffect, createContext, useContext } from 'react';
 
 const gameContext = createContext({});
 const gameToggleContext = createContext({});
 
 export function GameProvider({ children }) {
-    const [game, setGame] = useState({ started: false });
+	const [game, setGame] = useState({ started: false });
 
-    const startGame = () => {
-        if (!game.started) {
-            setGame((prevGame) => ({
-                ...prevGame,
-                started: true,
-            }));
-            console.log(game);
-        }
-    };
+	const startGame = () => {
+		if (!game.started) {
+			setGame((prevGame) => ({
+				...prevGame,
+				started: true,
+			}));
+		}
+	};
 
-    const selectDeck = (deckKey = "") => {
-        if (!game.deck) {
-            setGame((prevGame) => ({
-                ...prevGame,
-                deck: deckKey,
-            }));
-        }
-    };
+	const selectDeck = (deckKey = '') => {
+		if (!game.deck) {
+			setGame((prevGame) => ({
+				...prevGame,
+				deck: deckKey,
+			}));
+		}
+	};
 
-    const saveCards = (cards = []) => {
-        setGame((prevGame) => ({
-            ...prevGame,
-            cards: cards,
-        }));
-    };
+	const saveCards = (cards = []) => {
+		setGame((prevGame) => ({
+			...prevGame,
+			cards: cards,
+		}));
+	};
 
-    useEffect(() => {
-        console.log(game);
-    }, [game]);
-    return (
-        <gameContext.Provider value={game}>
-            <gameToggleContext.Provider
-                value={{
-                    startGame: startGame,
-                    selectDeck: selectDeck,
-                    saveCards: saveCards,
-                }}
-            >
-                {children}
-            </gameToggleContext.Provider>
-        </gameContext.Provider>
-    );
+	return (
+		<gameContext.Provider value={game}>
+			<gameToggleContext.Provider
+				value={{
+					startGame: startGame,
+					selectDeck: selectDeck,
+					saveCards: saveCards,
+				}}>
+				{children}
+			</gameToggleContext.Provider>
+		</gameContext.Provider>
+	);
 }
 
 export const useGameContext = () => useContext(gameContext);
